017a97f1ed4d9a8d8c37fa6356665c22191ad163
Yongcheng Wu <yc_wu1030@126.com>
2019-09-02 14:09:30 -0400

    Upload to github

---
+++ 2HDM_CPC_Type-II_UFO/CT_couplings.py
@@ -0,0 +1,11 @@
+# This file was automatically created by FeynRules 2.3.32
+# Mathematica version: 10.4.1 for Mac OS X x86 (64-bit) (April 17, 2016)
+# Date: Mon 2 Sep 2019 12:33:36
+
+
+from object_library import all_couplings, Coupling
+
+from function_library import complexconjugate, re, im, csc, sec, acsc, asec, cot
+
+
+
