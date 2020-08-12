5c0893c8a88f14198625468019bf554f8ca3b905
Yongcheng Wu <yc_wu1030@126.com>
2020-08-12 15:35:00 -0400

    Clean, Update
    Put model files into corresponding folders
    Update the version information

---
+++ UFOs/Type-I/2HDM_CPC_ggF_Type-I_UFO/coupling_orders.py
@@ -0,0 +1,16 @@
+# This file was automatically created by FeynRules 2.3.32
+# Mathematica version: 10.4.1 for Mac OS X x86 (64-bit) (April 17, 2016)
+# Date: Mon 2 Sep 2019 19:09:53
+
+
+from object_library import all_orders, CouplingOrder
+
+
+QCD = CouplingOrder(name = 'QCD',
+                    expansion_order = 99,
+                    hierarchy = 1)
+
+QED = CouplingOrder(name = 'QED',
+                    expansion_order = 99,
+                    hierarchy = 2)
+
