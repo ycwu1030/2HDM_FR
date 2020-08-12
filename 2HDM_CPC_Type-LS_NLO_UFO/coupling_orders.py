11b1895c0aa99d840f3d0e9cf8cc8b2f8d478006
Yongcheng Wu <yc_wu1030@126.com>
2020-08-12 15:27:51 -0400

    Add Type-I, LS, FL
    Add NLO model file for Type-I, LS, FL

---
+++ 2HDM_CPC_Type-LS_NLO_UFO/coupling_orders.py
@@ -0,0 +1,17 @@
+# This file was automatically created by FeynRules 2.3.36
+# Mathematica version: 11.3.0 for Mac OS X x86 (64-bit) (March 7, 2018)
+# Date: Wed 12 Aug 2020 14:54:51
+
+
+from object_library import all_orders, CouplingOrder
+
+
+QCD = CouplingOrder(name = 'QCD',
+                    expansion_order = 99,
+                    hierarchy = 1,
+                    perturbative_expansion = 1)
+
+QED = CouplingOrder(name = 'QED',
+                    expansion_order = 99,
+                    hierarchy = 2)
+
