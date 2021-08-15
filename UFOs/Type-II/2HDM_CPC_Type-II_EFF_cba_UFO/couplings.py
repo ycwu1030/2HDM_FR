026e5c1423807067decee2ef7fc5028d1cb54e37
Yongcheng Wu <yc_wu1030@126.com>
2021-08-15 17:34:51 -0500

    fix(couplings):
    Add extra factor (-i) for some missing vertices

--- UFOs/Type-II/2HDM_CPC_Type-II_EFF_cba_UFO/couplings.py
+++ UFOs/Type-II/2HDM_CPC_Type-II_EFF_cba_UFO/couplings.py
@@ -1055,31 +1055,31 @@ GC_ggZbot = Coupling(name = 'GC_ggZbot',
                       order = {'QCD':2,'QED':1, 'NLOZ':1})
 
 GC_ggHLHLtop = Coupling(name = 'GC_ggHLHLtop',
-                      value = 'G**2*kHLUp*kHLUp/(8.*cmath.pi**2*vev**2)',
+                      value = '-complex(0,1)*G**2*kHLUp*kHLUp/(8.*cmath.pi**2*vev**2)',
                       order = {'QCD':2,'QED':2, 'NLOB':1})
 
 GC_ggHLHLbot = Coupling(name = 'GC_ggHLHLbot',
-                      value = 'G**2*kHLDo*kHLDo/(8.*cmath.pi**2*vev**2)',
+                      value = '-complex(0,1)*G**2*kHLDo*kHLDo/(8.*cmath.pi**2*vev**2)',
                       order = {'QCD':2,'QED':2, 'NLOB':1})
 
 GC_ggHLHHtop = Coupling(name = 'GC_ggHLHHtop',
-                      value = 'G**2*kHLUp*kHHUp/(8.*cmath.pi**2*vev**2)',
+                      value = '-complex(0,1)*G**2*kHLUp*kHHUp/(8.*cmath.pi**2*vev**2)',
                       order = {'QCD':2,'QED':2, 'NLOB':1})
 
 GC_ggHLHHbot = Coupling(name = 'GC_ggHLHHbot',
-                      value = 'G**2*kHLDo*kHHDo/(8.*cmath.pi**2*vev**2)',
+                      value = '-complex(0,1)*G**2*kHLDo*kHHDo/(8.*cmath.pi**2*vev**2)',
                       order = {'QCD':2,'QED':2, 'NLOB':1})
 
 GC_ggHHHHtop = Coupling(name = 'GC_ggHHHHtop',
-                      value = 'G**2*kHHUp*kHHUp/(8.*cmath.pi**2*vev**2)',
+                      value = '-complex(0,1)*G**2*kHHUp*kHHUp/(8.*cmath.pi**2*vev**2)',
                       order = {'QCD':2,'QED':2, 'NLOB':1})
 
 GC_ggHHHHbot = Coupling(name = 'GC_ggHHHHbot',
-                      value = 'G**2*kHHDo*kHHDo/(8.*cmath.pi**2*vev**2)',
+                      value = '-complex(0,1)*G**2*kHHDo*kHHDo/(8.*cmath.pi**2*vev**2)',
                       order = {'QCD':2,'QED':2, 'NLOB':1})
 
 GC_ggHAHAtop = Coupling(name = 'GC_ggHAHAtop',
-                      value = 'G**2*kHAUp*kHAUp/(8.*cmath.pi**2*vev**2)',
+                      value = '-complex(0,1)*G**2*kHAUp*kHAUp/(8.*cmath.pi**2*vev**2)',
                       order = {'QCD':2,'QED':2, 'NLOB':1})
 
 GC_ggHAHAbot = Coupling(name = 'GC_ggHAHAbot',
