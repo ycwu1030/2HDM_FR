8ba54d41e522569416b2bb77697e23a9c84a3550
Yongcheng Wu <yc_wu1030@126.com>
2025-09-11 22:41:35 +0800

    rename Generic Yukawa coupling UFO outputs

---
+++ Tools/Patch_Generic.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+LoopFileDir="Loop_Induced_Vertices"
+
+VertexTypes=("ggS" "ggZ" "ggSS" "gagaS")
+YukawaTypes=("Generic")
+
+for ytype in ${YukawaTypes[@]}
+  do
+    SourceDir=UFOs/${ytype}/2HDM_CPC_${ytype}_cba_UFO
+    TargetDir=UFOs/${ytype}/2HDM_CPC_${ytype}_EFF_cba_UFO
+    rm -rf $TargetDir
+    cp -r $SourceDir $TargetDir
+    mkdir -p $TargetDir/Fortran
+    touch $TargetDir/Fortran/functions.f
+    cat $LoopFileDir/parameters_${ytype}.py >> $TargetDir/parameters.py
+    cat $LoopFileDir/coupling_orders.py >> $TargetDir/coupling_orders.py
+    for vtype in ${VertexTypes[@]}
+        do
+            cat $LoopFileDir/couplings_$vtype.py >> $TargetDir/couplings.py
+            cat $LoopFileDir/lorentz_$vtype.py >> $TargetDir/lorentz.py
+            cat $LoopFileDir/vertices_$vtype.py >> $TargetDir/vertices.py
+            cat $LoopFileDir/Fortran/functions_$vtype.f >> $TargetDir/Fortran/functions.f
+        done
+  done
